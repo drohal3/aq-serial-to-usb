380b6cc6238f81754abd8e14cd27eb66c2ab76dd
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-08-19 12:04:47 +0300

    data separation

--- tramsmitter/main.py
+++ tramsmitter/main.py
@@ -1,9 +1,16 @@
 import serial
 import time
+import json
 
 PORT = "/dev/ttyS0"
+
+data = {
+    "device_id": "test_0",
+    "temperature": 25.6,
+    "humidity": 70
+}
 def transmit(n: int = 1):
-    port = serial.Serial(
+    ser = serial.Serial(
         port=PORT,
         baudrate=9600,
         parity=serial.PARITY_NONE,
@@ -11,11 +18,18 @@ def transmit(n: int = 1):
         bytesize=serial.EIGHTBITS,
         timeout=3.0
     )
-    for i in range(n):
-        message = f"Hello {i}"
-        port.write(message.encode("utf-8"))
-    else:
-        print("Transmission completed!")
+    try:
+        for i in range(n):
+            message = {**data, "n": n}
+            json_message = json.dumps(message)
+            ser.write((json_message + '\n').encode("utf-8"))
+            time.sleep(1)
+        else:
+            print("Transmission completed!")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        ser.close()
 
 
 if __name__ == '__main__':
